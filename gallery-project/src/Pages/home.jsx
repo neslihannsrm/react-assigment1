da7de1bb9783b73a66b8d26f0b69a917dd8e63f0
neslihannsrm <neslihannsrm@gmail.com>
2025-02-03 23:56:36 +0100

    use statement practice

--- gallery-project/src/Pages/home.jsx
+++ gallery-project/src/Pages/home.jsx
@@ -3,22 +3,46 @@ import {useState} from 'react'
 
 
 function Home() {
-
-    const [name, setName] = useState ();
+    
+    const [name, setName] = useState("guest");
+    const [age, setAge] = useState(0); 
+    const [isEmployed, setIsEmployed] = useState(false);
 
 
     const updateName = () => {
-        setName("matilda");
+        setName("Matilda");
     }
+    
+    const IncrementAge  = () => {
+        setAge(age+ 1);
+    }
+
+    const toggleEmployment = () => {
+        if (isEmployed) {
+            setIsEmployed(false);
+        } else {
+            setIsEmployed(true);
+        }
+    }
+
+
     return (
-
         <div>
-            <button className="home-btn" onClick={updateName}>
-               Set name
+            <button className='home-btn'onClick={updateName}> 
+                Set name!
             </button>
-            <h2>Name: {name} </h2>
-        </div>
+            <h2>Name: {name}</h2>
 
+            <button className='home-btn'onClick={IncrementAge}>
+                IncrementAge 
+            </button>
+            <h2>Age: {age} </h2>
+
+            <button className='home-btn'onClick={toggleEmployment}>
+                Toggle
+            </button>
+            <h2>isEmployed: {isEmployed ? "Yes" : "No" }  </h2>
+        </div>
     );
 }
 export default Home;
