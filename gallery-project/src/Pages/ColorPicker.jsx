7b9da7cc4054cbc244d04ce47323d6e9e5b88e92
neslihannsrm <neslihannsrm@gmail.com>
2025-02-12 00:17:59 +0100

    added color picker page jsx and css

--- gallery-project/src/Pages/ColorPicker.jsx
+++ gallery-project/src/Pages/ColorPicker.jsx
@@ -1,2 +1,25 @@
 import React, { useState } from 'react';
 import '../index.css';
+
+function ColorPicker() {
+
+    const [color,serColor] = useState ("#FFFFFF");
+   
+    function handleColorChange(e){
+        serColor(e.target.value)
+    }
+    return (
+        <div className='color-picker-container'>
+            <h1 className='color-picker-h1'>Color Picker</h1>
+            <div className="color-display" style={{backgroundColor: color}}>
+                <p>Select a Color: {color}</p>
+            </div>
+            <label>Select a Color</label>
+            <input type="color" value={color} onChange={handleColorChange}/> 
+        
+        </div>
+    );
+  }
+  
+  export default ColorPicker;
+  
