5b2f374e4f48482acc5e48f96f8b58aed0b0255c
neslihannsrm <neslihannsrm@gmail.com>
2025-02-13 00:41:02 +0100

    added increment and decrement buttons

--- gallery-project/src/Pages/Contact.jsx
+++ gallery-project/src/Pages/Contact.jsx
@@ -3,7 +3,7 @@ import '../index.css';
 
 function Contact() {
   const [name, setName] = useState('');
-  const [age, setAge] = useState('');
+  const [age, setAge] = useState(1);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -11,7 +11,7 @@ function Contact() {
     if (name && age) {
       alert(`Welcome, ${name}! Your age is: ${age}`);
       setName('');
-      setAge('');
+      setAge(1);
     } else {
       alert('Please fill in all fields!');
     }
@@ -19,15 +19,23 @@ function Contact() {
 
   const handleReset = () => {
     setName('');
-    setAge('');
+    setAge(1);
+  };
+
+  const incrementAge = () => {
+    setAge((prevAge) => prevAge + 1);
+  };
+
+  const decrementAge = () => {
+    setAge((prevAge) => (prevAge > 1 ? prevAge - 1 : 1));
   };
 
   return (
     <div className="form-container">
-      <h2 className='login-title'>Login </h2>
+      <h2 className='login-title'>Login</h2>
       <form onSubmit={handleSubmit}>
         <div className="input-container">
-          <label htmlFor="name">Name:</label>
+          <label htmlFor="name">Name: </label>
           <input
             type="text"
             id="name"
@@ -36,16 +44,20 @@ function Contact() {
           />
         </div>
         <div className="input-container">
-          <label htmlFor="age">Age:</label>
-          <input
-            type="number"
-            id="age"
-            value={age}
-            onChange={(e) => setAge(e.target.value)}
-            min="1"
-          />
+        <div className="age-input-container">
+          <label htmlFor="age">Age: </label>
+          
+            <input
+              type="number"
+              id="age"
+              value={age}
+              onChange={(e) => setAge(Number(e.target.value))}
+              min="1"
+            />
+            <button type="button" onClick={incrementAge} className="age-button">+</button>
+            <button type="button" onClick={decrementAge} className="age-button">-</button>
+          </div>
         </div>
-
         <div className="button-container">
           <button type="submit" className="submit-button">Log In</button>
           <button type="button" onClick={handleReset} className="reset-button">Reset</button>
